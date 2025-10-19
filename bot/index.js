6892aaf668f1d651ee89dad6f4f72a0d498d4cf2
khaledahmed0918-sys <khaledahmed0918@gmail.com>
2025-10-19 20:06:15 +0300

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -3,6 +3,9 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const express = require("express");
+const app = express();
+
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
